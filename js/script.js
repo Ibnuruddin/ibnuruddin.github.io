e16b4f9f4328431b2fee44d7ae691f54dfc7881a
Ibnuruddin <kucingkutelukabehlemu2@gmail.com>
2022-11-03 21:28:58 +0700

    My Portfolio
    Belajar membuat Web Portfolio

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$('.page-scroll').on('click', function(e) {
+
+    var tujuan = $(this).attr('href');
+   
+    var elemenTujuan = $(tujuan);
+   
+    $('html').animate({
+     scrollTop: elemenTujuan.offset().top -60
+    }, 1000, 'swing');
+   
+    e.preventDefault();
+   });
+
+   //parallax
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    // jumbotron
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+    });
+
+    // portfolio
+    if(wScroll > $('.portfolio').offset().top -250){
+       $('.portfolio .img-thumbnail').each(function(i){
+        setTimeout(function(){
+            $('.portfolio .img-thumbnail').eq(i).addClass('muncul');
+        }, 300 * (i+1));
+       });
+       
+     
+    }
+
+});
